d9fcec4bf136141f720957787a7b200164d74daa
Truyen Nguyen <haohaothieugia@gmail.com>
2022-08-23 23:00:57 +0700

    first commit
    Add Readme file

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,4 +1,13 @@
-function helloWorld() {
-  console.log("Hello world");
+import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
+import Heading from "./components/heading/heading.js";
+const helloWorldButton = new HelloWorldButton();
+const heading = new Heading();
+helloWorldButton.render();
+heading.render("cács");
+
+if (process.env.NODE_ENV === "production") {
+  console.log("Prod mode");
+} else if (process.env.NODE_ENV === "development") {
+  console.log("Dev mod");
 }
-export default helloWorld;
+heading.nothing();
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import helloWorld from "./hello-world.js";
-import addImage from "./add-image.js";
-helloWorld();
-addImage();
